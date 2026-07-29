f19473f88a864258172abd01540b2d6724bec728
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2026-07-29 11:55:41 -0300

    implementando novos métodos

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -7,3 +7,20 @@ int num1 = 5;
 int num2 = 10;
 
 Console.WriteLine($"{num1} + {num2} = {calculadora.Somar(num1, num2)}");
+
+Console.WriteLine($"{num1} - {num2} = {calculadora.Subtrair(num1, num2)}");
+
+Console.WriteLine($"{num1} * {num2} = {calculadora.Multiplicar(num1, num2)}");
+
+try
+{
+    Console.WriteLine($"{num1} / {num2} = {calculadora.Dividir(num1, num2)}");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
+
+Console.WriteLine($"O número {num1} é par? {calculadora.EhPar(num1)}");
+
+Console.WriteLine($"O número {num2} é par? {calculadora.EhPar(num2)}");
--- Calculadora/Services/CalculadoraImp.cs
+++ Calculadora/Services/CalculadoraImp.cs
@@ -12,6 +12,23 @@ namespace Calculadora.Services
             return num1 + num2;
         }
 
+        public int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Dividir(int num1, int num2)
+        {
+            if (num2 == 0)
+                throw new ArgumentException("O divisor não pode ser zero.");
+            return (double) num1 / num2;
+        }
+
         public bool EhPar(int num)
         {
             return num % 2 == 0;
